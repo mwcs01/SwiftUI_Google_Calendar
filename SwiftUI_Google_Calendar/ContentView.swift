1443b6b5daf18d485e815f455bce72498f5a9773
Jim Baughman <jim.baughman@stmarcus.org>
2019-12-18 17:29:48 -0600

    Initial Commit

---
+++ SwiftUI_Google_Calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Google_Calendar
+//
+//  Created by DB Admin on 12/18/19.
+//  Copyright © 2019 Jim Baughman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
